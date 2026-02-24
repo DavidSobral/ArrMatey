729f6423a5e1317db202b8014f6aa0f4cb8849ac
Owen LeJeune <omlejeune@gmail.com>
2026-02-24 10:43:03 -0500

    updates for izzyondroid publishing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,6 +91,12 @@ android {
     buildFeatures {
         buildConfig = true
     }
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs (for IzzyOnDroid/F-Droid)
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles (for Google Play)
+        includeInBundle = false
+    }
 }
 
 dependencies {
