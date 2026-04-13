43a431c94781e02587543ac5674355cc84aff453
Owen LeJeune <omlejeune@gmail.com>
2026-04-13 16:31:10 -0400

    update android version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -76,7 +76,7 @@ android {
         applicationId = "com.dnfapps.arrmatey"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
+        versionCode = 12
         versionName = "0.3.0"
     }
     packaging {
